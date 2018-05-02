71333f2a68918763eb5fc0543c4b02541425e084
m-gall <mailielgall@gmail.com>
2018-05-02 13:14:03 +1000

    change to accept 3 samples

--- plot-histo.py
+++ plot-histo.py
@@ -7,36 +7,51 @@ import plotly.graph_objs as go
 from plotly.graph_objs import Scatter, Layout
 
 
-def parse_data(filename):
-    samples = None
+def parse_data(filename1, filename2, filename3):
 
-    with open(filename) as tsvfile:
-        reader = csv.reader(tsvfile, delimiter='\t')
+    data_combined = []
+    non_zeros_combined = []
+    samples_combined = []
 
-        header = next(reader)
-        samples = header[6:]
+    files = [filename1, filename2, filename3]
 
-        data = []
-        non_zeros = []
-        for row in reader:
-            data.append(row[6:])
-            for val in row[6:]:
-                if val == '':
-                    val = float(0)
-                    data.append(val)
-                else:
-                    val = float(val)
-                    non_zeros.append(val)
-                    data.append(val)
+    for filename in files:
 
-    tsvfile.close()
+        with open(filename) as tsvfile:
+            reader = csv.reader(tsvfile, delimiter='\t')
 
-    return [non_zeros, data, samples]
+            header = next(reader)
+            samples = header[6:]
+
+            data = []
+            non_zeros = []
+            for row in reader:
+                data.append(row[6:])
+                for val in row[6:]:
+                    if val == '':
+                        val = float(0)
+                        data.append(val)
+                    else:
+                        val = float(val)
+                        non_zeros.append(val)
+                        data.append(val)
+
+        data_combined.append(data)
+        non_zeros_combined.append(non_zeros)
+        samples_combined.append(samples)
+
+        tsvfile.close()
+
+    return [non_zeros_combined, data_combined, samples_combined]
 
 
-def generate_histo(non_zeros):
-    trace2 = go.Histogram(x=non_zeros)
-    data = [trace2]
+def generate_histo(non_zeros1, nonzeros2, nonzeros3):
+
+    trace1 = go.Histogram(x=non_zeros1)
+    trace2 = go.Histogram(x=nonzeros2)
+    trace3 = go.Histogram(x=nonzeros3)
+
+    data = [trace1, trace2, trace3]
 
     return data
 
@@ -50,8 +65,8 @@ def get_layout_attributes(plot_title, xlabel, ylabel):
         yaxis=dict(
             title=ylabel
         ),
-        bargap=0.2,
-        bargroupgap=0.1
+        bargap=0,
+        bargroupgap=0
     )
 
     return layout
@@ -80,19 +95,24 @@ if __name__ == '__main__':
 
     non_zeros = []
     filename = sys.argv[1]
-    # filename = 'mmr-fcctx-adjusted-merged-canon.tsv'
+    filename1 = 'data/mmr-fcctx-adjusted-merged-canon.tsv'
+    filename2 = 'data/fcctx-adjust-merged-canon.tsv'
+    filename3 = 'data/mmr-adjust-merged-canon.tsv'
+
     outfilename = 'mmr_fcctx_snp_histo.html'
 
     plot_title = 'MMR and FCCTX merged-canonized'
     plot_xlabel = 'Confidence'
     plot_ylabel = 'Count'
 
-    tempdata = parse_data(filename)
+    tempdata = parse_data(filename1, filename2, filename3)
+
     non_zeros = tempdata[0]
     zeros = tempdata[1]
     samples = tempdata[2]
 
-    data_histo = generate_histo(non_zeros)
+    data_histo = generate_histo(non_zeros[0], non_zeros[1], non_zeros[2])
+
     layout = get_layout_attributes(plot_title, plot_xlabel, plot_ylabel)
 
     data_heatmap = generate_heatmap(non_zeros, samples)
