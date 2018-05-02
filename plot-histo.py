a7d3930d10f0b11f83e7e9b3c613f3f6ba88f158
m-gall <mailielgall@gmail.com>
2018-05-02 12:42:13 +1000

    updates

--- plot-histo.py
+++ plot-histo.py
@@ -8,6 +8,7 @@ from plotly.graph_objs import Scatter, Layout
 
 
 def parse_data(filename):
+    samples = None
 
     with open(filename) as tsvfile:
         reader = csv.reader(tsvfile, delimiter='\t')
@@ -22,27 +23,25 @@ def parse_data(filename):
             for val in row[6:]:
                 if val == '':
                     val = float(0)
+                    data.append(val)
                 else:
                     val = float(val)
                     non_zeros.append(val)
+                    data.append(val)
 
     tsvfile.close()
 
-    return non_zeros
+    return [non_zeros, data, samples]
 
 
 def generate_histo(non_zeros):
-
     trace2 = go.Histogram(x=non_zeros)
     data = [trace2]
-
 
     return data
 
 
 def get_layout_attributes(plot_title, xlabel, ylabel):
-
-
     layout = go.Layout(
         title=plot_title,
         xaxis=dict(
@@ -55,17 +54,17 @@ def get_layout_attributes(plot_title, xlabel, ylabel):
         bargroupgap=0.1
     )
 
-
     return layout
 
-def generate_heatmap(non_zeros, outfilename):
 
-    trace = go.Heatmap(z=data,
+def generate_heatmap(non_zeros, samples):
+
+    trace = go.Heatmap(z=non_zeros,
                        x=samples,
                        y=samples, colorscale='Greys')
     data = [trace]
 
-    return
+    return data
 
 
 
@@ -79,24 +78,26 @@ if __name__ == '__main__':
         print("Usage: <input tsv file name>")
         sys.exit(0)
 
-
-    non_zeros =[]
+    non_zeros = []
     filename = sys.argv[1]
-   # filename = 'mmr-fcctx-adjusted-merged-canon.tsv'
-    outfilename = 'fcctx_snp_histo.html'
+    # filename = 'mmr-fcctx-adjusted-merged-canon.tsv'
+    outfilename = 'mmr_fcctx_snp_histo.html'
 
-    plot_title= 'MMR and FCCTX merged-canonized'
+    plot_title = 'MMR and FCCTX merged-canonized'
     plot_xlabel = 'Confidence'
     plot_ylabel = 'Count'
 
-    non_zeros = parse_data(filename)
-    data = generate_histo(non_zeros)
+    tempdata = parse_data(filename)
+    non_zeros = tempdata[0]
+    zeros = tempdata[1]
+    samples = tempdata[2]
+
+    data_histo = generate_histo(non_zeros)
     layout = get_layout_attributes(plot_title, plot_xlabel, plot_ylabel)
 
-    fig = go.Figure(data=data, layout=layout)
+    data_heatmap = generate_heatmap(non_zeros, samples)
 
+    fig = go.Figure(data=data_histo, layout=layout)
 
-    # py.offline.plot(data, filename='fcctx_snp_similarity.html')
-    py.offline.plot(fig, filename=outfilename)
-
-
+    # py.offline.plot(data, filename='plots/'+ 'fcctx_snp_similarity.html')
+    py.offline.plot(fig, filename='plots/' + outfilename)
